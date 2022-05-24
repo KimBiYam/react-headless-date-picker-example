f946d357e288b0f212e5481a507ae10c034f9746
ChangHyunKim <hot9998@naver.com>
2022-05-25 06:14:37 +0900

    Temporary

--- src/App.js
+++ src/App.js
@@ -1,23 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useDatePicker } from "react-headless-date-picker";
 
 function App() {
+  const [dateStates, setDateStates] = useState({
+    focus: null,
+    date: null,
+  });
+
+  const handleDateChange = (state) => {
+    console.log(state);
+    setDateStates(state);
+  };
+
+  const {
+    activatedMonths,
+    goToPreviousMonth,
+    goToNextMonth,
+    monthDays,
+    weekdayLabels,
+    onDayClick,
+    isDisabledDate,
+  } = useDatePicker({
+    focus: dateStates.focus,
+    onDateChange: handleDateChange,
+    selectedDate: dateStates.date,
+  });
+
+  console.log(dateStates.focus);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <div>
+        <strong>focus :</strong>
+        {`${dateStates.focus}`}
+      </div>
+      <div>
+        <strong>selectedDate :</strong>
+        {dateStates.date && dateStates.date.toLocaleString()}
+      </div>
+      <button type="button" onClick={goToPreviousMonth}>
+        prev
+      </button>
+      <button type="button" onClick={goToNextMonth}>
+        next
+      </button>
+      <div
+        style={{
+          display: "grid",
+          margin: "32px 0 0",
+          gridTemplateColumns: `repeat(${activatedMonths.length}, 300px)`,
+          gridGap: "0 64px",
+        }}
+      >
+        {activatedMonths.map((month) => (
+          <div key={month.date}>
+            <strong
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                margin: "16px 0",
+              }}
+            >
+              {month.label}
+            </strong>
+            <div
+              style={{
+                display: "grid",
+                gridTemplateColumns: "repeat(7, 1fr)",
+                justifyContent: "center",
+              }}
+            >
+              {weekdayLabels.map((dayLabel) => (
+                <div style={{ textAlign: "center" }} key={dayLabel}>
+                  {dayLabel}
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(7, 1fr)",
+            justifyContent: "center",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {monthDays.map((monthDay) =>
+            monthDay.map((day, index) => {
+              if (day === null) {
+                return <div key={index}></div>;
+              }
+
+              return (
+                <button
+                  type="button"
+                  key={day.date}
+                  onClick={() => onDayClick(day.date)}
+                  disabled={isDisabledDate(day.date)}
+                >
+                  {day.label}
+                </button>
+              );
+            })
+          )}
+        </div>
+      </div>
     </div>
   );
 }
